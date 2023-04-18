2eefa495ab994a5a61d8e36d42f0143c31180f0d
OctavioCortez24 <oc31549@gmail.com>
2023-04-17 23:17:38 -0300

    Test

--- Class.js
+++ Class.js
@@ -1,45 +1,45 @@
-class user{
-    constructor(name, surname, email, password, dateOfBirth){
-        this.name=name;
-        this.surname=surname;
-        this.email=email;
-        this.password=password;
-        this.dateOfBirth=dateOfBirth;
+class user {
+    constructor(name, surname, email, password, dateOfBirth) {
+        this.name = name;
+        this.surname = surname;
+        this.email = email;
+        this.password = password;
+        this.dateOfBirth = dateOfBirth;
     }
-    setName(name){this.name=name;}
-    getName(){return this.name;}
-    setSurname(surname){this.surname=surname;}
-    getSurname(){return this.surname;}
-    setEmail(email){this.email=email;}
-    getEmail(){return this.email;}
-    setPassword(password){this.password=password;}
-    getPassword(){return this.password;}
-    setDateOfBirth(dateOfBirth){this.dateOfBirth=dateOfBirth;}
-    getDateOfBirth(){return this.dateOfBirth;}
+    setName(name) { this.name = name; }
+    getName() { return this.name; }
+    setSurname(surname) { this.surname = surname; }
+    getSurname() { return this.surname; }
+    setEmail(email) { this.email = email; }
+    getEmail() { return this.email; }
+    setPassword(password) { this.password = password; }
+    getPassword() { return this.password; }
+    setDateOfBirth(dateOfBirth) { this.dateOfBirth = dateOfBirth; }
+    getDateOfBirth() { return this.dateOfBirth; }
 }
 // creo clases para el alquilr de vehiculos Lunes 17/4
-class alquiler{
-    constructor(nombre,email,telefono,tipoDeAutomovil,fechaAdquisitiva,fechaDevolucion){
-this.nombre=nombre;
-this.email=email;
-this.telefono=telefono;
-this.tipoDeAutomovil=tipoDeAutomovil;
-this.fechaAdquisitiva=fechaAdquisitiva;
-this.fechaDevolucion=fechaDevolucion;
+class alquiler {
+    constructor(nombre, email, telefono, tipoDeAutomovil, fechaAdquisitiva, fechaDevolucion) {
+        this.nombre = nombre;
+        this.email = email;
+        this.telefono = telefono;
+        this.tipoDeAutomovil = tipoDeAutomovil;
+        this.fechaAdquisitiva = fechaAdquisitiva;
+        this.fechaDevolucion = fechaDevolucion;
 
     }
-setNombre(nombre){this.nombre=nombre;}
-getNombre(){return this.nombre;}
-setEmail(email){this.email=email;}
-getEmail(){return this.email;}
-setTelefono(telefono){this.telefono=telefono}
-getTelefono(){return this.telefono;}
-setTipoDeAutomovil(tipoDeAutomovil){this.tipoDeAutomovil=tipoDeAutomovil}
-getTipoDeAutomovil(){return this.tipoDeAutomovil}
-setFechaAdquisitiva(fechaAdquisitiva){this.fechaAdquisitiva=fechaAdquisitiva}
-getFechaAdquisitiva(){return this.fechaAdquisitiva}
-setFechaDevolucion(fechaDevolucion){this.fecha=fechaDevolucion}
-getFechaDevolucion(){return this.fecha}
+    setNombre(nombre) { this.nombre = nombre; }
+    getNombre() { return this.nombre; }
+    setEmail(email) { this.email = email; }
+    getEmail() { return this.email; }
+    setTelefono(telefono) { this.telefono = telefono }
+    getTelefono() { return this.telefono; }
+    setTipoDeAutomovil(tipoDeAutomovil) { this.tipoDeAutomovil = tipoDeAutomovil }
+    getTipoDeAutomovil() { return this.tipoDeAutomovil }
+    setFechaAdquisitiva(fechaAdquisitiva) { this.fechaAdquisitiva = fechaAdquisitiva }
+    getFechaAdquisitiva() { return this.fechaAdquisitiva }
+    setFechaDevolucion(fechaDevolucion) { this.fecha = fechaDevolucion }
+    getFechaDevolucion() { return this.fecha }
 
 }
-module.exports={user,alquiler}
+module.exports = { user, alquiler }
