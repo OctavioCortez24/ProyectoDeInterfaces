66302be18df56eeaffb1ef60c82a4f7f1c349589
OctavioCortez24 <oc31549@gmail.com>
2023-04-22 20:21:44 -0300

    Modifico el login

--- Routes/menu.routes.js
+++ Routes/menu.routes.js
@@ -1,13 +1,13 @@
 const express = require('express');
 const router= express.Router();
-
+//const {tieneAcceso()}=require('../')
 
 const controller=require('../Controller/menu.controller')
 //Menu principal
 router.get('/', controller.menu)
 router.get('/menu', controller.menuPrincipal)
-router.get('/Consulta', controller.Consulta)
+router.get('/alquiler', controller.alquiler)
 router.get('/compras',controller.compras)
-router.post('/ConsultaPost',controller.ConsultaPost)
+router.post('/alquilerPost',controller.alquilerPost)
 
 module.exports=router;
